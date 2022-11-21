8d12159ed3408a9b4a7021e27c6450e4662e7431
Himadhith <79003240+Himadhith@users.noreply.github.com>
2022-11-21 09:43:31 +0530

    Add files via upload
    Data to populate DB

---
+++ Backend/data/likes.sql
@@ -0,0 +1,10 @@
+insert into likes (username, tweetid, timeStamp_l) values ('jphizackarley6', '20', '5/31/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('ahymas1', '13', '11/13/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('mjoynes0', '9', '9/23/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('afallawe9', '2', '12/18/2021');
+insert into likes (username, tweetid, timeStamp_l) values ('piamittii8', '15', '12/12/2021');
+insert into likes (username, tweetid, timeStamp_l) values ('cgallaher3', '10', '1/1/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('tglisane5', '17', '10/9/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('jhearsey2', '7', '9/27/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('eretchless7', '1', '11/4/2022');
+insert into likes (username, tweetid, timeStamp_l) values ('cnother4', '6', '12/28/2021');
---
+++ Backend/data/login_record.sql
@@ -0,0 +1,10 @@
+insert into login_record (username, timestamp_t) values ('mjoynes0', '11/16/2021');
+insert into login_record (username, timestamp_t) values ('tglisane5', '5/13/2022');
+insert into login_record (username, timestamp_t) values ('eretchless7', '5/22/2022');
+insert into login_record (username, timestamp_t) values ('jphizackarley6', '1/19/2022');
+insert into login_record (username, timestamp_t) values ('afallawe9', '6/23/2022');
+insert into login_record (username, timestamp_t) values ('cgallaher3', '6/18/2022');
+insert into login_record (username, timestamp_t) values ('piamittii8', '8/27/2022');
+insert into login_record (username, timestamp_t) values ('jhearsey2', '4/6/2022');
+insert into login_record (username, timestamp_t) values ('ahymas1', '4/11/2022');
+insert into login_record (username, timestamp_t) values ('cnother4', '7/12/2022');
